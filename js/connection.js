5d3f417df3cf415ad5e5001bbaa11888a181da49
JoseJavierGJ <jjosejavier@hotmail.com>
2023-11-05 22:38:08 -0600

    Validación de Inicio de Sesión

---
+++ js/connection.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-gCK1I47pf6RsZwVTeBZhPUwG7ImaPbM",
+  authDomain: "yojada-is.firebaseapp.com",
+  projectId: "yojada-is",
+  storageBucket: "yojada-is.appspot.com",
+  messagingSenderId: "1021229416164",
+  appId: "1:1021229416164:web:5eddc0a13be778710cb6f1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
